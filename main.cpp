a9ece7c5b0b971cba0d1fcf620f52bcdb73a8aa7
Emiliano Carrillo Moncayo <EmilianoCarrilloMoncayo@gmail.com>
2017-12-05 09:55:23 -0600

    final one main

--- main.cpp
+++ main.cpp
@@ -1,13 +1,93 @@
 #include <iostream>
-#include <cstdlib>
+#include <stdlib.h>
 #include <ctime>
 #include <conio.h>
 #include <Windows.h>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+
+
 using namespace std;
 
-enum dir { ARRIBA, ABAJO, IZQUIERDA, DERECHA };
+enum dir { ARRIBA, ABAJO, DERECHA, IZQUIERDA , NADA};
 
+// CLASE SNAKE
+class Snake {
+private:
+	int xCuerpo[100], yCuerpo[100];
+	int longitud;
+	char caracter, caracterCabeza;
+public:
+	Snake() {
+		longitud = 1;
+		caracter = 'o';
+		caracterCabeza = 'O';
+	}
+	void avanzar(dir direccionTecla);
+	void crecer();
+	void posicionarAlCentro(int anchura, int altura);
+	int getXCuerpo(int index) {
+		return xCuerpo[index];
+	}
+	int getYCuerpo(int index) {
+		return yCuerpo[index];
+	}
+	int getLongitud() { return longitud; }
+	void setLongitud(int longitud) { this->longitud = longitud;  }
+	char getCaracter() { return caracter; }
+	char getCaracterCabeza() { return caracterCabeza; }
+};
+	// Métodos offline de Snake
+void Snake::avanzar(dir direccionTecla) {
+	if (direccionTecla == ARRIBA) {
+		for (int i = longitud - 1; i >= 0; i--) {
+			if (i == 0) yCuerpo[0]--;
+			else {
+				yCuerpo[i] = yCuerpo[i - 1];
+				xCuerpo[i] = xCuerpo[i - 1];
+			}
+		}
+	}
+	if (direccionTecla == ABAJO) {
+		for (int i = longitud - 1; i >= 0; i--) {
+			if (i == 0) yCuerpo[0]++;
+			else {
+				yCuerpo[i] = yCuerpo[i - 1];
+				xCuerpo[i] = xCuerpo[i - 1];
+			}
+		}
+	}
+	if (direccionTecla == IZQUIERDA) {
+		for (int i = longitud - 1; i >= 0; i--) {
+			if (i == 0) xCuerpo[0]--;
+			else {
+				xCuerpo[i] = xCuerpo[i - 1];
+				yCuerpo[i] = yCuerpo[i - 1];
+			}
+		}
+	}
+	if (direccionTecla == DERECHA) {
+		for (int i = longitud - 1; i >= 0; i--) {
+			if (i == 0) xCuerpo[0]++;
+			else {
+				xCuerpo[i] = xCuerpo[i - 1];
+				yCuerpo[i] = yCuerpo[i - 1];
+			}
+		}
+	}
+}
+void Snake::crecer() { longitud++; }
+
+void Snake::posicionarAlCentro(int anchura, int altura) {
+	xCuerpo[0] = anchura / 2;
+	yCuerpo[0] = altura / 2;
+}
+
+// CLASE TABLERO
 class Tablero {
+private:
 	int anchura, altura;
 	char caracterBorde;
 public:
@@ -16,143 +96,262 @@ public:
 		altura = 25;
 		caracterBorde = '#';
 	}
-	int getAltura() {return altura;}
-	int getAnchura() {return anchura;}
-	char getCaracterBorde() {return caracterBorde;}
+	int getAnchura() { return anchura; }
+	int getAltura() { return altura; }
+	char getCaracterBorde() { return caracterBorde; }
 };
 
-class Snake {
-	int xCuerpo[100], yCuerpo[100];
-	int xCabeza = xCuerpo[0], yCabeza = yCuerpo[0];
-	int longitud;
-	char caracter;
-	bool cuerpoImpresoTotalmente = false;
-public:
-	Snake() {
-		longitud = 1;
-		caracter = 'O';
-	}
-	void posicionarEnELCentro(int anchura, int altura) {
-		xCuerpo[0] = anchura / 2;
-		yCuerpo[0] = altura / 2;
-	}
-	int getXCuerpo(int index) { return xCuerpo[index]; }
-	int getYCuerpo(int index) { return yCuerpo[index]; }
-	/*Añadí estas dos*/
-	/*EDITARLAS Y CREAR UNAS DE MOVIMIENTO (RECORRIDO EN EL ARRAY)*/
-	void setXCuerpo(int val) { xCuerpo[0] = val; }
-	void setYCuerpo(int val) { yCuerpo[0] = val; }
-	/*---------------*/
-	int getLongitud() { return longitud; }
-	char getCaracter() { return caracter; }
-	bool impresoTotalmente() { return cuerpoImpresoTotalmente; }
-	void setCuerpoImpresoTotalmente(bool val) { cuerpoImpresoTotalmente = val; }
-};
-
+// CLASE COMIDA
 class Comida {
+private:
 	int x, y;
 	char caracter;
 public:
 	Comida() {
 		caracter = '@';
 	}
-	void posicionarAleatoriamente(int anchura, int altura) {
-		int xAleatorio = rand() % (anchura - 2) + 1; //Número aleatorio entre 1 y anchuraTablero-2
-		int yAleatorio = rand() % (altura - 2) + 1; //Número aleatorio entre 1 y anchuraTablero-2
-		this->x = xAleatorio;
-		this->y = yAleatorio;
-	}
+	void posicionarAleatoriamente(int anchura, int altura);
 	int getX() { return x; }
 	int getY() { return y; }
 	char getCaracter() { return caracter; }
 };
 
+	// Métodos offline de Comida
+void Comida::posicionarAleatoriamente(int anchura, int altura) {
+	x = rand() % (anchura-2 - 1 + 1) + 1;
+	y = rand() % (altura-2 - 1 + 1) + 1;
+	
+}
+
+//CLASE JUEGO
 class Juego {
-	Tablero tablero;
+private:
 	Snake culebrin;
+	Tablero tablero;
 	Comida manzana;
+	bool gameOver;
 	dir direccionTecla;
+	string nombreUsuario;
+	int decisionJugar;
 public:
-	void setup() {
-		manzana.posicionarAleatoriamente(tablero.getAnchura(), tablero.getAltura());
-		culebrin.posicionarEnELCentro(tablero.getAnchura(), tablero.getAltura());
-	}
-	void dibujar() {
-		system("cls");
-		int cantidadBolasImpresas = 0;
-		culebrin.setCuerpoImpresoTotalmente(false);
-		for (int y = 0; y < tablero.getAltura(); y++){
-			for (int x = 0; x < tablero.getAnchura(); x++){
-				//Imprimir bordes
-				if (x == 0 || y == 0 || x == tablero.getAnchura() - 1 || y == tablero.getAltura() - 1) cout << tablero.getCaracterBorde();
-				//Imprimir Comida
-				else if (x == manzana.getX() && y == manzana.getY()) cout << manzana.getCaracter();
-				//Imprimir Snake
-				else {
-					bool bolaImpresa = false;
-					for (int i = 0; (i < culebrin.getLongitud() && !culebrin.impresoTotalmente()); i++){
-						if (x == culebrin.getXCuerpo(i) && y == culebrin.getYCuerpo(i)) {
-							cout << culebrin.getCaracter();
-							bolaImpresa = true;
-							cantidadBolasImpresas++;
-							if (cantidadBolasImpresas == culebrin.getLongitud()) culebrin.setCuerpoImpresoTotalmente(true);
-						}
-					}
-					//Imprimir espacios vacíos
-					if (!bolaImpresa) cout << ' ';
-				}
-			}
-			cout << endl;
-		}
-	}
-	void recibirInputTeclado(){
-		if (_kbhit()) {
-			switch (_getch()) {
-				case 'w':
-					direccionTecla = ARRIBA;
-					culebrin.setYCuerpo(2);
-					cout << "w" << culebrin.getYCuerpo(0) << endl;
-					break;
-				case 'a':
-					direccionTecla = IZQUIERDA;
-					cout << "a" << endl;
-					break;
-				case 's':
-					direccionTecla = ABAJO;
-					cout << "s" << endl;
-					break;
-				case 'd':
-					direccionTecla = DERECHA;
-					cout << "d" << endl;
-					break;
-			}
-		}
-	}
-
-	void cambiarEstado() {
-		manzana.posicionarAleatoriamente(tablero.getAnchura(), tablero.getAltura());
-		if (direccionTecla == ARRIBA) culebrin.setYCuerpo(5); //PRUEBA
-		if (direccionTecla == ABAJO) culebrin.setYCuerpo(10); //PRUEBA
-		if (direccionTecla == IZQUIERDA) culebrin.setXCuerpo(5); //PRUEBA
-		if (direccionTecla == DERECHA) culebrin.setXCuerpo(10); //PRUEBA
-
-
-	}
-
+	void setup();
+	void mostrarPantallaInicio();
+	void mostrarPantallaGameOver();
+	void guardarPuntajeEnBD();
+	void recibirInputTeclado();
+	void cambiarEstado();
+	void dibujar();
+	Juego() : decisionJugar(1) {}
+	bool getGameOver() { return gameOver; }
+	int getDecisionJugar() { return decisionJugar; }
 };
 
+	// Métodos offline de Juego
+void Juego::setup() {
+	gameOver = false;
+	direccionTecla = NADA;
+	culebrin.setLongitud(1);
+	manzana.posicionarAleatoriamente(tablero.getAnchura(), tablero.getAltura());
+	culebrin.posicionarAlCentro(tablero.getAnchura(), tablero.getAltura());
+}
+
+void Juego::mostrarPantallaInicio() {
+	system("cls");
+	string renglon;
+	ifstream asciiInicio("asciiArtInicio.txt");
+
+	if (asciiInicio.is_open()){
+		while (!asciiInicio.eof()) {
+			getline(asciiInicio, renglon);
+			cout << renglon << endl;
+		}
+		asciiInicio.close();
+	}else {
+		cout << "\t\t\t\tERROR: Archivo no existente." << endl << endl;
+	}
+	
+
+	cout << "\t\t\t\t Nombre: ";
+	cin >> nombreUsuario;
+	cout << "\t\t\t\t Inserta una moneda." << endl;
+	cin.get();
+	cin.get();
+}
+
+void Juego::mostrarPantallaGameOver() {
+	string renglon;
+	int contador = 1;
+	int index = 1;
+	int puntajes[50];
+	string nombres[50];
+
+	ifstream scoreboard;
+	scoreboard.open("scoreboard.txt");
+
+	if (scoreboard.is_open()) {
+		while (!scoreboard.eof()) {
+			getline(scoreboard, renglon);
+			if (contador % 2 != 0) {
+				istringstream (renglon) >> puntajes[index];
+				index++;
+			}
+			else {
+				nombres[index - 1] = renglon;
+			}
+			contador++;
+		}
+
+		int i, key, j;
+		string key2;
+		for (i = 1; i < contador/2; i++){
+			key = puntajes[i];
+			key2 = nombres[i];
+			j = i - 1;
+			while (j >= 0 && puntajes[j] > key){
+				puntajes[j + 1] = puntajes[j];
+				nombres[j + 1] = nombres[j];
+				j = j - 1;
+			}
+			puntajes[j + 1] = key;
+			nombres[j + 1] = key2;
+		}
+	}else {
+		cout << "\t\t\t\tERROR: Archivo no existente." << endl << endl;
+	}
 
 
+	// MOSTRAR ASCII ART GAME OVER
+	system("cls");
+	ifstream asciiGameOver("asciiArtGameOver.txt");
+
+	if (asciiGameOver.is_open()){
+		while (!asciiGameOver.eof()) {
+			getline(asciiGameOver, renglon);
+			cout << "\t\t\t" << renglon << endl;
+		}
+	} else {
+		cout << "\t\t\t\tERROR: Archivo no existente." << endl << endl;
+	}
+	
+	cout << "\n\n\t\t\t\t\t\tTU PUNTAJE: " << culebrin.getLongitud() - 1;
+
+	cout << "\n\n\t\t\t\t\t**********SCOREBOARD**********" << endl;
+	cout << "\t\t\t\t\t*                            *" << endl;
+	int posicion = 1;
+	for (int i = contador/2-1; i >= (contador/2)-3 ; i--) {
+		cout << "\t\t\t\t\t*\t  "  << posicion << ". " << puntajes[i] << " " << nombres[i] << "\t     *" << endl;
+		posicion++;
+	}
+	cout << "\t\t\t\t\t*                            *" << endl;
+	cout << "\t\t\t\t\t******************************" << endl;
+
+	cout << endl << endl << "\t\t\t\t\t    Deseas volver a jugar?" << endl;
+	cout << "\t\t\t\t\t\t1. SI\n\t\t\t\t\t\t2. NO, SALIR" << endl;
+	cout << "\t\t\t\t\t\t:: ";
+	cin >> decisionJugar;
+
+	cin.get();
+}
+
+void Juego::guardarPuntajeEnBD() {
+	//GUARDAR EN "BASE DE DATOS"
+	ofstream scoreboard;
+	scoreboard.open("scoreboard.txt", ios_base::app);
+
+	scoreboard << culebrin.getLongitud() - 1 << endl << nombreUsuario << endl;
+
+	scoreboard.close();
+}
+
+void Juego::recibirInputTeclado() {
+	if (_kbhit()) {
+		switch (_getch()) {
+		case 'w':
+			if (direccionTecla == ABAJO) direccionTecla = ABAJO;
+			else direccionTecla = ARRIBA;
+			break;
+		case 'a':
+			if (direccionTecla == DERECHA) direccionTecla = DERECHA;
+			else direccionTecla = IZQUIERDA;
+			break;
+		case 's':
+			if (direccionTecla == ARRIBA) direccionTecla = ARRIBA;
+			else direccionTecla = ABAJO;
+			break;
+		case 'd':
+			if (direccionTecla == IZQUIERDA) direccionTecla = IZQUIERDA;
+			else direccionTecla = DERECHA;
+			break;
+		}
+	}
+}
+
+void Juego::cambiarEstado() {
+	culebrin.avanzar(direccionTecla);
+	//Si las coordenadas de la cabeza de culebrin son iguales a la de la comida
+	//Culebrin crecerá y la manzana deberá posicionarse en otro lugar aleatorio.
+	if (culebrin.getXCuerpo(0) == manzana.getX() && culebrin.getYCuerpo(0) == manzana.getY()) {
+		culebrin.crecer();
+		manzana.posicionarAleatoriamente(tablero.getAnchura(), tablero.getAltura());
+	}
+	//Morir si toca bordes
+	if ( culebrin.getXCuerpo(0) == tablero.getAnchura()-1 || culebrin.getYCuerpo(0) == tablero.getAltura() - 1 || culebrin.getXCuerpo(0) == 0 || culebrin.getYCuerpo(0) == 0)	{
+		gameOver = true;
+	}
+	// Morir si toca su cuerpo
+	for (int i = 4; i < culebrin.getLongitud(); i++){
+		if (culebrin.getXCuerpo(0) == culebrin.getXCuerpo(i) && culebrin.getYCuerpo(0) == culebrin.getYCuerpo(i)) 
+			gameOver = true;
+	}
+}
+
+void Juego::dibujar() {
+	bool bandera = false;
+	system("cls");
+	// Dibujar Tablero
+	for (int y = 0; y < tablero.getAltura(); y++) {
+		for (int x = 0; x < tablero.getAnchura(); x++) {
+			//Checar y dibujar el borde del tablero
+			if (y == 0 || x == 0 || y == tablero.getAltura() - 1 || x == tablero.getAnchura() - 1) {
+				cout << tablero.getCaracterBorde();
+			// Checar y dibujar manzana
+			} else if (x == manzana.getX() && y == manzana.getY()) {
+				cout << manzana.getCaracter();
+			} else {
+			// Checar y dibujar culebra entera
+				for (int i = 0; i < culebrin.getLongitud(); i++) {
+					if (x == culebrin.getXCuerpo(i) && y == culebrin.getYCuerpo(i)){
+						bandera = true;
+						if (i == 0) cout << culebrin.getCaracterCabeza();
+						else cout << culebrin.getCaracter();
+					}
+				}
+				// De lo contrario, imprimir espacio
+				if (bandera == false) cout << " "; 
+				bandera = false;
+			}
+		}
+		cout << endl ;
+	}
+	cout << "PUNTAJE: " << culebrin.getLongitud() - 1;
+}
 
 
 
 int main() {
-	srand(time(0));
+	srand(time(NULL));
 	Juego juego;
-	juego.setup();
-	while (true) {
-		juego.dibujar();
-		juego.recibirInputTeclado();
-		juego.cambiarEstado(); 
+
+	while (juego.getDecisionJugar() == 1) {
+		juego.setup();
+		juego.mostrarPantallaInicio();
+		while (!juego.getGameOver()) {
+			juego.recibirInputTeclado();
+			juego.cambiarEstado();
+			juego.dibujar();
+			Sleep(15);
+		}
+		juego.guardarPuntajeEnBD();
+		juego.mostrarPantallaGameOver();
 	}
 }
